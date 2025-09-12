125252376fe3fa499bc7d7d8f2308082b1df2c2c
MomoMizu94 <jamppa.sortsi@gmail.com>
2025-09-12 21:44:22 +0300

    New paths for status bar scripts.

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", ". /usr/local/bin/sb-music",                           2,              17},
-    //{"", ". /usr/local/bin/sb-record",                          2,                13},
-    {"", ". /usr/local/bin/price btc Bitcoin  ",           60,             11},
-    {"", ". /usr/local/bin/price eth Ethereum  ",          60,             12},
-    {"󰻠 ", ". /usr/local/bin/cpu-temp",	                            2,             14},
-    {"  ", ". /usr/local/bin/mem-usage",                            4,             15},
-    {"  ", ". /usr/local/bin/gpu-temp",                             2,             16},
-    {"", ". /usr/local/bin/weather",                                3600,           2},
-    {"  ", ". /usr/local/bin/clock",                               10,             1},
+	/*Icon*/	/*Command*/		        /*Update Interval*/	/*Update Signal*/
+    {"",    "sb-music",                             2,      17},
+    {"",    "price btc Bitcoin  ",                 60,     11},
+    {"",    "price eth Ethereum  ",                60,     12},
+    {"󰻠 ",  "cpu-temp",	                            2,      14},
+    {"  ", "mem-usage",                            4,      15},
+    {"  ", "gpu-temp",                             2,      16},
+    {"",    "weather",                              3600,    2},
+    {"  ", "clock",                                10,      1},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
